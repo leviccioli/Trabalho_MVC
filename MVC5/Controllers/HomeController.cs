fb76fc615496e3b1113927b7a98279c906a3d244
Leticia Viccioli <le.bviccioli@live.com>
2017-11-20 21:35:40 -0200

    InicioMVC
    Inicio do trabalho mvc

---
+++ MVC5/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sua descrição sobre a Página";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contatos da página";
+
+            return View();
+        }
+    }
+}
