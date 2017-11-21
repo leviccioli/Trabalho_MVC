efc06b8cca75884ac7b516591cbc5e177c917ac1
Leticia Viccioli <le.bviccioli@live.com>
2017-11-21 20:07:48 -0200

    Model
    Criação das class model

---
+++ MVC5/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using MVC5.Model;
+
+namespace MVC5.Controllers
+{
+    public class ClienteController : Controller
+    {
+        Contexto contexto = new Contexto();
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View(contexto.Cliente.ToList());
+        }
+    }
+}
---
+++ MVC5/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace MVC5.Model
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("MVC5") { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<OS> OrdemServico { get; set; }
+
+    }
+}
